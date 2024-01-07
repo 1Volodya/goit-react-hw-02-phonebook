dccd19ed9a3100132ff541ef2e9ba3b78f4aa2b1
Volodymyr Koval <vovankoval1996@gmail.com>
2024-01-07 02:41:32 +0200

    done 2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    number: '',
+    filter: '',
+  };
+
+  // handleChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
+  handleChangeFilter = event => {
+    const value = event.target.value;
+    this.setState({ filter: value });
+  };
+
+  handleAppProfile = formData => {
+    if (this.state.contacts.some(profile => profile.name === formData.name)) {
+      alert(`Profile with this ${formData.name} alredy axists!`);
+      return;
+    }
+
+    const finaleProfile = { ...formData, id: nanoid() };
+    console.log(finaleProfile);
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, finaleProfile],
+      };
+    });
+  };
+
+  handleDeleteContact = contactId => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== contactId),
+    });
+  };
+
+  render() {
+    const filteredProfiles = this.state.contacts.filter(profile =>
+      profile.name
+        .toLowerCase()
+        .includes(this.state.filter.trim().toLowerCase())
+    );
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          handleAppProfile={this.handleAppProfile}
+          onChange={this.handleChange}
+        />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} onChange={this.handleChangeFilter} />
+        <ContactList
+          contacts={(this.state.contacts, filteredProfiles)}
+          onDelete={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
